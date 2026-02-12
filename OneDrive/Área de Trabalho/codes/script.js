0d804d6caf165ef73a778b6f2baad6184c20f8ea
Gabriel Cavalcante <seu.email@gmail.com>
2026-02-12 16:42:33 -0300

    criaçao de cadastro de membros

--- OneDrive/Área de Trabalho/codes/script.js
+++ OneDrive/Área de Trabalho/codes/script.js
@@ -33,6 +33,8 @@ function fecharSolicitacao() {
 window.onclick = function(event) {
     var modalMembros = document.getElementById("modalMembros");
     var modalSolicitacao = document.getElementById("nova-solicitacao");
+    var modalAta = document.getElementById("Enviar-Ata");
+    let modalCad = document.getElementById("modalCadastro");
 
     // Se clicou no fundo escuro dos Membros - Fecha Membros
     if (event.target == modalMembros) {
@@ -43,7 +45,55 @@ window.onclick = function(event) {
     if (event.target == modalSolicitacao) {
         modalSolicitacao.style.display = "none";
     }
+    
+    // Se clicou no fundo escuro da Ata - Fecha Ata
+    if (event.target == modalAta) {
+        modalAta.style.display = "none";
+    }
+    if (event.target == modalCad) {
+        modalCad.style.display = "none";
+    }
 }
+
 function abrirAta() {
-    document.getElementById("modalAta").style.display = "flex";
+    var modalAta = document.getElementById("Enviar-Ata");
+    if (modalAta) {
+        modalAta.style.display = "flex";
+    } else {
+        console.error("Erro: Não encontrei a div com id 'Enviar-Ata'");
+    }
+    fecharModal(); // Fecha a janela de membros, se estiver aberta
+    fecharSolicitacao(); // Fecha a janela de nova solicitação, se estiver aberta
 }
+function fecharAta() {
+      var modal = document.getElementById("Enviar-Ata");
+    if (modal) {
+        modal.style.display = "none";
+    }
+}
+ function alertaAta(){
+    alert("Ata enviada com sucesso!");
+ }
+ function alertaSolicitacao(){
+    alert("Solicitação enviada com sucesso!");
+ }
+function alertaMembros(){
+    alert("Membros adicionados com sucesso!");
+}
+// Função para abrir o modal de Cadastro
+function abrirCadastro() {
+    document.getElementById("modalCadastro").style.display = "flex";
+}
+
+// Função para fechar
+function fecharCadastro() {
+    document.getElementById("modalCadastro").style.display = "none";
+}
+
+// Função simulada de salvar
+function salvarCadastro() {
+    alert("Usuário cadastrado com sucesso!");
+    fecharCadastro();
+}
+
+
